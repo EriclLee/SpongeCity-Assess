713804b946993fa0d2683950cb8ad27d5b4a9dac
sabermai <szhao.sabermai@gmail.com>
2016-05-23 11:06:09 +0800

    controllers and activities

--- SpongeCity-Assess-Net/SpongeCity.Assess.Web/Controllers/ActivitiesController.cs
+++ SpongeCity-Assess-Net/SpongeCity.Assess.Web/Controllers/ActivitiesController.cs
@@ -43,7 +43,98 @@ namespace SpongeCity.Assess.Web.Controllers
             return PartialView(report);
         }
 
+        //地下水水位
         public ActionResult GroundWaterLevel(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //生态岸线：盖度、存活率
+        public ActionResult EcologicalShorelineCoverAndSurvival(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //生态岸线：物种丰富度、岸线长度
+        public ActionResult EcologicalShorelineRichAndLength(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //地表水环境质量：参数1
+        public ActionResult RiverWaterEnvironmentParam1(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //地表水环境质量：参数2
+        public ActionResult RiverWaterEnvironmentParam2(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //地表水环境质量：水质参数
+        public ActionResult RiverWaterEnvironmentQuality(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //城市面源污染
+        public ActionResult NPSP(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //污水再生利用率
+        public ActionResult WaterwaterReuseRate(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //管网漏损
+        public ActionResult PipeLeakageRate(int subCategoryId, int viewId, int kpiId, int categoryId)
+        {
+            ViewBag.SubCategoryId = subCategoryId;
+            ViewBag.ViewId = viewId;
+            ViewBag.KpiId = kpiId;
+            ViewBag.CategoryId = categoryId;
+            return PartialView();
+        }
+
+        //雨水资源利用率
+        public ActionResult RainwaterUtilization(int subCategoryId, int viewId, int kpiId, int categoryId)
         {
             ViewBag.SubCategoryId = subCategoryId;
             ViewBag.ViewId = viewId;
